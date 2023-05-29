c0df241584768417f7a055ebaa6be9fae8199bbe
arielsasson <69323248+arielsasson@users.noreply.github.com>
2023-05-29 20:56:21 -0300

    Prueba viewmodel

--- app/src/main/java/com/example/parcialtp3/fragments/PerfilFragment.kt
+++ app/src/main/java/com/example/parcialtp3/fragments/PerfilFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.parcialtp3.R
+import com.example.parcialtp3.model.viewmodel.LoginViewModel
 
 class PerfilFragment : Fragment() {
-
+    lateinit var view: View
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var texto_usuario: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,7 +23,13 @@ class PerfilFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel = ViewModelProvider(requireActivity())[LoginViewModel::class.java]
+        view = inflater.inflate(R.layout.fragment_perfil, container, false)
+        texto_usuario = view.findViewById(R.id.txtNameProfile)
+        texto_usuario.text = viewModel.getUsuario().toString()
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        return view
     }
 }
